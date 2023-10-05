3d6ed2346e98d5a70e35a9deda20cb85c58960f5
rakib2010 <rakib.hasan.asia@gmail.com>
2023-10-05 13:34:54 +0600

    demo ecommerce app

---
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -0,0 +1,72 @@
+//
+// Generated file. Do not edit.
+// This file is generated from template in file `flutter_tools/lib/src/flutter_plugins.dart`.
+//
+
+// @dart = 2.18
+
+import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:local_auth_android/local_auth_android.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:local_auth_ios/local_auth_ios.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+import 'package:local_auth_windows/local_auth_windows.dart';
+
+@pragma('vm:entry-point')
+class _PluginRegistrant {
+
+  @pragma('vm:entry-point')
+  static void register() {
+    if (Platform.isAndroid) {
+      try {
+        LocalAuthAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`local_auth_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        AndroidWebViewPlatform.registerWith();
+      } catch (err) {
+        print(
+          '`webview_flutter_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+    } else if (Platform.isIOS) {
+      try {
+        LocalAuthIOS.registerWith();
+      } catch (err) {
+        print(
+          '`local_auth_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        WebKitWebViewPlatform.registerWith();
+      } catch (err) {
+        print(
+          '`webview_flutter_wkwebview` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+    } else if (Platform.isLinux) {
+    } else if (Platform.isMacOS) {
+    } else if (Platform.isWindows) {
+      try {
+        LocalAuthWindows.registerWith();
+      } catch (err) {
+        print(
+          '`local_auth_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+    }
+  }
+}
